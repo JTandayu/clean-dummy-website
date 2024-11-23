4fe88d276c07102b5166d935a8bf55ae42f94bea
JTandayu <jose.tandayu@gmail.com>
2024-11-24 01:52:00 +0800

    all commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function Home() {
+  return (
+    <Flex direction="column" gap="2">
+			<Text>Hello from Radix Themes :)</Text>
+			<Button>Let's go</Button>
+		</Flex>
+  );
+}
